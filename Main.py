5eccdf2e7facceebfe7a0517a4cf4393d24cbe67
chen <dotomato@163.com>
2017-12-12 09:28:49 +0800

    延时15秒 增加enter_car, comfir_car机制

--- Main.py
+++ Main.py
@@ -57,6 +57,12 @@ def send_enter_car_signal():
     requests.post(url, payload)
 
 
+def send_comfir_car_signal():
+    payload = json.dumps({'type': 'port', 'id': SELF_ID})
+    url = 'http://%s/api/v0.01/comfir_car' % SERVER_IP
+    requests.post(url, payload)
+
+
 def check_ir():
     while True:
         k = 0
@@ -65,6 +71,13 @@ def check_ir():
                 k += 1
         if k >= 5 and status == PORT_OPEN:  # 如果有大于等于5个红外检测到距离为0（也就是5个或者6个都符合），并且当前状态为OPEN
             send_enter_car_signal()
+            time.sleep(15)
+            k2 = 0
+            for gpio in GPIO_IN_List:  # 计算有多个少红外检测到距离为0
+                if GPIO.input(gpio) == 0:
+                    k2 += 1
+            if k>= 5:
+                send_comfir_car_signal()
         time.sleep(1)  # 每秒检测一次
 
 
